26819fe6a982935836d36fdd7bec2dac65d53e8f
balajisivakumar <text2balaji@gmail.com>
2024-06-17 08:29:37 +0100

    update

--- caseSensitive.js
+++ caseSensitive.js
@@ -1,14 +1,14 @@
-var name = "masai";
+var names = "masai";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var output = "";
 
-for(var i=0; i<name.length; i++)
+for(var i=0; i<names.length; i++)
 {
 	for(var j=0;j<lower.length; j++)
 	{
-		if(name[i] == lower[j])
+		if(names[i] == lower[j])
 		{
 			output = output + upper[j];
 		}
